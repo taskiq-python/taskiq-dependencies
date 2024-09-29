28f2236a5c5f0f892fe5378b2b17ba8df3bb09e3
Pavel Kirilin <win10@list.ru>
2024-09-29 23:17:11 +0200

    Updated dependencies.

--- taskiq_dependencies/__init__.py
+++ taskiq_dependencies/__init__.py
@@ -6,6 +6,7 @@ in your project easily.
 
 Github repo: https://github.com/taskiq-python/taskiq-dependencies
 """
+
 from taskiq_dependencies.dependency import Depends
 from taskiq_dependencies.graph import DependencyGraph
 from taskiq_dependencies.utils import ParamInfo
